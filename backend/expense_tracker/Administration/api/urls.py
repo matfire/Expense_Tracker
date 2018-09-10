a6c9a427108de7c51d4e2ca4c7fb6b6bd7fe4fcc
Matteo <mgassend@gmail.com>
2018-09-10 16:20:03 +0200

    removed frontend

--- backend/expense_tracker/Administration/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path("create/", views.RegistrationAPI.as_view()),
-    path("login/", views.Login),
-    # path("", views.UserAPI.as_view()),
-]
--- backend/expense_tracker/Budget/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
-
-admin.site.register(models.Inlet_Category)
-admin.site.register(models.Inlet)
